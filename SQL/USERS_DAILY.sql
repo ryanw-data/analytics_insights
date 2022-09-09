496a634eb36dc6f765bfd624499a9a2d8390441f
Ryan Wyatt <ryanwyatt1992@gmail.com>
2022-09-09 20:17:38 +0100

    updated SQL to new logic

--- SQL/USERS_DAILY.sql
+++ SQL/USERS_DAILY.sql
@@ -1,89 +1,105 @@
 WITH USER_UPDATES AS (
+    --Returns all "user licence update" events types from the commercial site
     SELECT 
-    CASE WHEN sr.ID IN (8) THEN 'Unlicenced' ELSE 'Licenced' END LICENCE_TYPE,
+    CASE WHEN sr.ID IN (8) THEN 'Unlicenced' ELSE 'Licenced' END LICENCE_STATUS, --if not 8 unlicenced then must be licenced
     su.EMAIL USER_EMAIL,
     su.ID SYSTEM_USER_ID,
     he.CREATED_AT CHANGE_TIME,
-    DATE_TRUNC('day',he.created_at)::DATE CHANGE_DATE
+    DATE_TRUNC('day',he.CREATED_AT)::DATE CHANGE_DATE, --removing the timestamp from CHANGE_TIME
+    ROW_NUMBER() OVER(PARTITION BY su.EMAIL ORDER BY he.CREATED_AT) rn --order of events per user
     FROM HISTORICAL_EVENTS he
-    JOIN HIST_USERS u ON u.ID = he.hist_target_user_id
-    JOIN SYSTEM_USERS su ON su.ID = u.system_user_id
-    JOIN SITE_ROLES sr ON sr.ID = u.site_role_id
-    WHERE HISTORICAL_EVENT_TYPE_ID = 18
-    AND he.hist_target_site_id = 5
+    JOIN HIST_USERS u ON u.ID = he.HIST_TARGET_USER_ID
+    JOIN SYSTEM_USERS su ON su.ID = u.SYSTEM_USER_ID
+    JOIN SITE_ROLES sr ON sr.ID = u.SITE_ROLE_ID
+    WHERE he.HISTORICAL_EVENT_TYPE_ID = 18 --only "user licence update" event types
+    AND he.HIST_TARGET_SITE_ID = 5 --only commercial type
 ),
-CREATED_USERS AS (
+EVENT_START_DATE AS (
+    --Returns the first event time and date for use in later CTEs
     SELECT 
-    CASE 
-        WHEN NOT EXISTS (SELECT * FROM USER_UPDATES uu WHERE uu.USER_EMAIL = su.EMAIL) THEN CASE WHEN u.SITE_ID IN (8) THEN 'Unlicenced' ELSE 'Licenced' END
-        ELSE 'Licenced' END LICENCE_TYPE,
+    MIN(CREATED_AT) MIN_CHANGE_TIME,
+    DATE_TRUNC('day',MIN(CREATED_AT))::DATE MIN_CHANGE_DATE
+    FROM HISTORICAL_EVENTS
+),
+START_EVENT AS (
+    --If the first event is unlicenced then creates a fake licenced row to act as the starting point
+    SELECT
+    'Licenced' LICENCE_STATUS, --Assume that must have been licenced if first event is unlicenced
+    USER_EMAIL,
+    SYSTEM_USER_ID,
+    esd.MIN_CHANGE_TIME CHANGE_TIME,
+    esd.MIN_CHANGE_DATE CHANGE_DATE
+    FROM USER_UPDATES
+    CROSS JOIN EVENT_START_DATE esd
+    WHERE rn = 1 --only return the first update event for each user
+    AND LICENCE_STATUS = 'Unlicenced' --only return unlicenced events
+),
+LICENCED_USERS AS (
+    --Returns currently licenced users in the commercial site to act as the default list if no events exist
+    SELECT 
+    'Licenced' LICENCE_STATUS, --unlicenced type removed in where clause, everything else is varient on licenced
     su.EMAIL USER_EMAIL,
     su.ID SYSTEM_USER_ID,
-    u.CREATED_AT CREATE_TIME,
-    DATE_TRUNC('day',u.CREATED_AT)::DATE CREATE_DATE
+    esd.MIN_CHANGE_TIME CHANGE_TIME,
+    esd.MIN_CHANGE_DATE CHANGE_DATE
     FROM USERS u
     JOIN SYSTEM_USERS su ON su.ID = u.SYSTEM_USER_ID
-    WHERE u.SITE_ID = 5
-),
-CURRENT_USER_STATE AS (
-    SELECT 
-    CASE WHEN u.SITE_ROLE_ID IN (8) THEN 'Unlicenced' ELSE 'Licenced' END LICENCE_TYPE,
-    su.EMAIL USER_EMAIL,
-    su.ID SYSTEM_USER_ID,
-    'now'::timestamp CHANGE_TIME,
-    DATE_TRUNC('day','now'::timestamp)::DATE CHANGE_DATE
-    FROM USERS u
-    JOIN SYSTEM_USERS su ON su.ID = u.SYSTEM_USER_ID
-    WHERE u.SITE_ID = 5
+    CROSS JOIN EVENT_START_DATE esd
+    WHERE u.SITE_ID = 5 --only commercial type
+    AND u.SITE_ROLE_ID NOT IN (8) --removes "unlicenced" type
+    AND su.ID NOT IN (SELECT SYSTEM_USER_ID FROM USER_UPDATES) --only return users that have had no update events
 ),
 UNIONED_DATASET AS (
-SELECT *
+--Combines the three event CTEs into a single event stream and takes the latest event per day to create a grain of per user per day
+SELECT *, MIN(res_2.CHANGE_DATE) OVER(PARTITION BY res_2.USER_EMAIL ORDER BY res_2.CHANGE_DATE) FIRST_EVENT_DATE
 FROM (
-    SELECT res_1.*, cu.CREATE_DATE,
-    ROW_NUMBER() OVER(PARTITION BY res_1.USER_EMAIL, res_1.CHANGE_DATE ORDER BY res_1.CHANGE_TIME) rn
+    SELECT res_1.*,
+    ROW_NUMBER() OVER(PARTITION BY res_1.USER_EMAIL, res_1.CHANGE_DATE ORDER BY res_1.CHANGE_TIME) rn --latest row per day per user
     FROM (
-        SELECT *
+        SELECT LICENCE_STATUS, USER_EMAIL, SYSTEM_USER_ID, CHANGE_TIME, CHANGE_DATE
         FROM USER_UPDATES
         UNION ALL
-        SELECT *
-        FROM CURRENT_USER_STATE 
+        SELECT LICENCE_STATUS, USER_EMAIL, SYSTEM_USER_ID, CHANGE_TIME, CHANGE_DATE
+        FROM START_EVENT 
         UNION ALL
-        SELECT *
-        FROM CREATED_USERS
-        ORDER BY USER_EMAIL, CHANGE_DATE
+        SELECT LICENCE_STATUS, USER_EMAIL, SYSTEM_USER_ID, CHANGE_TIME, CHANGE_DATE
+        FROM LICENCED_USERS
     ) res_1
-    JOIN CREATED_USERS cu ON cu.SYSTEM_USER_ID = res_1.SYSTEM_USER_ID
 ) res_2
-WHERE rn = 1
-AND USER_EMAIL IS NOT NULL
+WHERE rn = 1 --only return the latest row per user per day
+AND USER_EMAIL IS NOT NULL --user must have an email
 ),
 DATE_RANGE AS (
+--Expands the event stream out to include a row per day per user even if an event did not happen
     SELECT *
     FROM (
         SELECT date_trunc('day', dd)::DATE DATES
-        FROM generate_series ('2015-01-01'::timestamp, 'now'::timestamp, '1 day'::interval) dd
+        FROM generate_series ('2015-01-01'::timestamp, 'now'::timestamp, '1 day'::interval) dd --creates a generic date series
     ) res_1
-    CROSS JOIN (SELECT DISTINCT USER_EMAIL, CREATE_DATE, SYSTEM_USER_ID FROM UNIONED_DATASET) x
-    WHERE x.CREATE_DATE <= res_1.DATES
+    CROSS JOIN (SELECT DISTINCT USER_EMAIL, FIRST_EVENT_DATE, SYSTEM_USER_ID FROM UNIONED_DATASET) x --Explodes out the dataset to create the grain
+    WHERE x.FIRST_EVENT_DATE <= res_1.DATES --only includes dates after the first event date
 )
-SELECT
-VIEW_DATE,
-FIRST_VALUE(LICENCE_TYPE) OVER(PARTITION BY USER_EMAIL, partition_window ORDER BY VIEW_DATE) LICENCE_TYPE,
-USER_EMAIL,
-SYSTEM_USER_ID,
-CREATE_DATE USER_CREATE_DATE, 
-CASE WHEN rn_desc = 1 THEN 1 ELSE 0 END CURRENT_STATUS_FLAG
+SELECT *
 FROM (
-    SELECT 
-    dr.DATES VIEW_DATE,
-    ud.LICENCE_TYPE,
-    dr.USER_EMAIL,
-    dr.SYSTEM_USER_ID,
-    dr.CREATE_DATE,
-    SUM(CASE WHEN ud.CREATE_DATE IS NULL THEN 0 ELSE 1 END) OVER(PARTITION BY dr.USER_EMAIL ORDER BY dr.DATES) partition_window,
-    ROW_NUMBER() OVER(PARTITION BY dr.USER_EMAIL ORDER BY dr.DATES DESC) rn_desc
-    FROM DATE_RANGE dr
-    LEFT JOIN UNIONED_DATASET ud ON dr.DATES = ud.CHANGE_DATE AND dr.USER_EMAIL = ud.USER_EMAIL
-    ORDER BY dr.user_email, dr.dates
-) res_1
+--Final output that includes all rows with backfill included
+    SELECT
+    VIEW_DATE,
+    FIRST_VALUE(LICENCE_STATUS) OVER(PARTITION BY USER_EMAIL, PARTITION_WINDOW ORDER BY VIEW_DATE) LICENCE_STATUS, --backfill value
+    USER_EMAIL,
+    SYSTEM_USER_ID,
+    CASE WHEN rn_desc = 1 THEN 1 ELSE 0 END CURRENT_DATE_FLAG --if the row is the most recent for user then 1
+    FROM (
+        --Subquery is to add all required columns into a single series per user per day and adds columns needed for backfill
+        SELECT 
+        dr.DATES VIEW_DATE,
+        ud.LICENCE_STATUS,
+        dr.USER_EMAIL,
+        dr.SYSTEM_USER_ID,
+        SUM(CASE WHEN ud.FIRST_EVENT_DATE IS NULL THEN 0 ELSE 1 END) OVER(PARTITION BY dr.USER_EMAIL ORDER BY dr.DATES) PARTITION_WINDOW, --incremental counter to act as a marker for backfill in outer query
+        ROW_NUMBER() OVER(PARTITION BY dr.USER_EMAIL ORDER BY dr.DATES DESC) rn_desc --row number to find latest row per user
+        FROM DATE_RANGE dr
+        LEFT JOIN UNIONED_DATASET ud ON dr.DATES = ud.CHANGE_DATE AND dr.USER_EMAIL = ud.USER_EMAIL
+    ) res_1
+) res_2
+WHERE LICENCE_STATUS = 'Licenced'
 ORDER BY USER_EMAIL, VIEW_DATE
--- SQL/USER_GROUPS.sql
+++ SQL/USER_GROUPS.sql
@@ -1,125 +1,112 @@
-WITH CURRENT_GROUP_MEMBERSHIPS AS (
+WITH GROUP_MEMBERSHIP_CHANGES AS (
+    --Returns all "user licence update" events types from the commercial site
+    SELECT 
+    su.ID SYSTEM_USER_ID,
+    su.email user_email, 
+    CASE WHEN he.HISTORICAL_EVENT_TYPE_ID = 42 THEN 'ADDED' ELSE 'REMOVED' END UPDATE_TYPE, --converting ID to text
+    he.CREATED_AT CHANGE_TIME,
+    DATE_TRUNC('day', he.CREATED_AT)::DATE CHANGE_DATE, --removing timestamp from CHANGE_TIME
+    hg.NAME GROUP_NAME,
+    ROW_NUMBER() OVER(PARTITION BY su.email, hg.NAME ORDER BY he.CREATED_AT) rn --order of events per user per group
+    FROM HISTORICAL_EVENTS he
+    JOIN HIST_USERS u ON u.ID = he.hist_target_user_id
+    JOIN SYSTEM_USERS su ON su.ID = u.system_user_id
+    JOIN HIST_GROUPS hg ON hg.ID = he.HIST_GROUP_ID
+    WHERE he.HISTORICAL_EVENT_TYPE_ID IN (42,43) --only add or remove from group events
+    AND he.HIST_TARGET_SITE_ID = 5 --only commercial site
+),
+EVENT_START_DATE AS (
+    --Returns the first event time and date from events table for use in later CTEs
+    SELECT 
+    MIN(CREATED_AT) MIN_CHANGE_TIME,
+    DATE_TRUNC('day', MIN(CREATED_AT))::DATE MIN_CHANGE_DATE
+    FROM HISTORICAL_EVENTS
+),
+START_EVENTS AS (
+    --If the first event is removed then creates a fake added row to act as the starting point
+    SELECT 
+    gmc.SYSTEM_USER_ID,
+    gmc.USER_EMAIL,
+    'ADDED' UPDATE_TYPE, --Assume that must have been in the group "added" if first event is removed
+    esd.MIN_CHANGE_TIME CHANGE_TIME,
+    esd.MIN_CHANGE_DATE CHANGE_DATE,
+    gmc.GROUP_NAME
+    FROM GROUP_MEMBERSHIP_CHANGES gmc
+    CROSS JOIN EVENT_START_DATE esd
+    WHERE gmc.rn = 1 --returns only first row per user per group
+    AND gmc.UPDATE_TYPE = 'REMOVED' --only rows where the user was removed from a group
+),
+GROUP_MEMBERSHIP AS (
+    --Returns currently group memberships in the commercial site to act as the default list if no events exist
     SELECT 
     su.ID SYSTEM_USER_ID,
     su.EMAIL USER_EMAIL,
-    'ADDED' UPDATE_TYPE,
-    'now'::timestamp CHANGE_TIME,
-    DATE_TRUNC('day', 'now'::timestamp)::DATE CHANGE_DATE,
+    'ADDED' UPDATE_TYPE, --Only active memberships exist in tables, so only one possible status
+    esd.MIN_CHANGE_TIME CHANGE_TIME,
+    esd.MIN_CHANGE_DATE CHANGE_DATE,
     g.NAME GROUP_NAME
     FROM GROUP_USERS gu
     JOIN USERS u ON u.ID = gu.USER_ID
     JOIN SYSTEM_USERS su ON su.ID = u.SYSTEM_USER_ID
     JOIN GROUPS g ON g.ID = gu.GROUP_ID 
-    WHERE gu.SITE_ID = 5
-),
-GROUP_MEMBERSHIP_CHANGES AS (
-    SELECT *
-    FROM (
-        SELECT 
-        su.ID SYSTEM_USER_ID,
-        su.email user_email, 
-        CASE WHEN he.HISTORICAL_EVENT_TYPE_ID = 42 THEN 'ADDED' ELSE 'REMOVED' END UPDATE_TYPE,
-        he.CREATED_AT CHANGE_TIME,
-        DATE_TRUNC('day', he.CREATED_AT)::DATE CHANGE_DATE,
-        hg.NAME GROUP_NAME,
-        ROW_NUMBER() OVER(PARTITION BY su.email, hg.NAME, DATE_TRUNC('day', he.CREATED_AT) ORDER BY he.CREATED_AT) day_rn,
-        ROW_NUMBER() OVER(PARTITION BY su.email, hg.NAME ORDER BY he.CREATED_AT) temp_rn
-        FROM HISTORICAL_EVENTS he
-        JOIN HIST_USERS u ON u.ID = he.hist_target_user_id
-        JOIN SYSTEM_USERS su ON su.ID = u.system_user_id
-        JOIN HIST_GROUPS hg ON hg.ID = he.HIST_GROUP_ID
-        WHERE he.HISTORICAL_EVENT_TYPE_ID IN (42,43)
-        AND he.HIST_TARGET_SITE_ID = 5
-    ) res_1
-    WHERE day_rn = 1
-),
-INITIAL_GROUP_STATE AS (
-    SELECT 
-    SYSTEM_USER_ID,
-    user_email,
-    CASE WHEN UPDATE_TYPE = 'ADDED' THEN 'REMOVED'
-         WHEN UPDATE_TYPE = 'REMOVED' THEN 'ADDED'
-         ELSE 'ADDED' END UPDATE_TYPE,
-    'now'::timestamp - '7 month'::interval  CHANGE_TIME,
-    DATE_TRUNC('day', 'now'::timestamp - '7 month'::interval)::DATE CHANGE_DATE,
-    GROUP_NAME
-    FROM GROUP_MEMBERSHIP_CHANGES
-    WHERE temp_rn = 1
-),
-CATCH_GROUPS_NEVER_CHANGED AS (
-    SELECT 
-    SYSTEM_USER_ID,
-    USER_EMAIL,
-    'ADDED' UPDATE_TYPE,
-    'now'::timestamp - '7 month'::interval CHANGE_TIME,
-    DATE_TRUNC('day', 'now'::timestamp - '7 month'::interval)::DATE CHANGE_DATE,
-    GROUP_NAME
-    FROM CURRENT_GROUP_MEMBERSHIPS cgm
-    WHERE NOT EXISTS (SELECT * FROM GROUP_MEMBERSHIP_CHANGES gmc WHERE gmc.USER_EMAIL = cgm.USER_EMAIL AND gmc.GROUP_NAME = cgm.GROUP_NAME)
+    CROSS JOIN EVENT_START_DATE esd
+    WHERE gu.SITE_ID = 5 --commercial site only
+    AND NOT EXISTS (SELECT * FROM GROUP_MEMBERSHIP_CHANGES gmc WHERE gmc.USER_EMAIL = su.EMAIL AND gmc.GROUP_NAME = g.NAME) --only if there is no rows in event CTE
 ),
 UNIONED_DATASETS AS (
-    SELECT 
-    SYSTEM_USER_ID,
-    USER_EMAIL,
-    UPDATE_TYPE,
-    CHANGE_TIME,
-    CHANGE_DATE,
-    GROUP_NAME
-    FROM CURRENT_GROUP_MEMBERSHIPS
-    UNION ALL
-    SELECT 
-    SYSTEM_USER_ID,
-    USER_EMAIL,
-    UPDATE_TYPE,
-    CHANGE_TIME,
-    CHANGE_DATE,
-    GROUP_NAME
-    FROM GROUP_MEMBERSHIP_CHANGES
-    UNION ALL
-    SELECT 
-    SYSTEM_USER_ID,
-    USER_EMAIL,
-    UPDATE_TYPE,
-    CHANGE_TIME,
-    CHANGE_DATE,
-    GROUP_NAME
-    FROM INITIAL_GROUP_STATE
-    UNION ALL
-    SELECT
-    SYSTEM_USER_ID,
-    USER_EMAIL,
-    UPDATE_TYPE,
-    CHANGE_TIME,
-    CHANGE_DATE,
-    GROUP_NAME
-    FROM CATCH_GROUPS_NEVER_CHANGED
+    --A combined dataset with all events ready to create a daily timeseries
+    SELECT res_2.*,
+    MIN(res_2.CHANGE_DATE) OVER(PARTITION BY res_2.USER_EMAIL, res_2.GROUP_NAME ORDER BY res_2.CHANGE_DATE) FIRST_EVENT_DATE --the first event for the user in the group, acts as start of timeseries
+    FROM (
+        SELECT res_1.*,
+        ROW_NUMBER() OVER(PARTITION BY res_1.USER_EMAIL, res_1.GROUP_NAME, res_1.CHANGE_DATE ORDER BY res_1.CHANGE_TIME) rn --ranks events per user per group per day
+        FROM (
+            --combines the event CTEs into a single stream per user per group
+            SELECT SYSTEM_USER_ID, USER_EMAIL, UPDATE_TYPE, CHANGE_TIME, CHANGE_DATE, GROUP_NAME
+            FROM GROUP_MEMBERSHIP_CHANGES
+            UNION ALL
+            SELECT SYSTEM_USER_ID, USER_EMAIL, UPDATE_TYPE, CHANGE_TIME, CHANGE_DATE, GROUP_NAME
+            FROM START_EVENTS
+            UNION ALL
+            SELECT SYSTEM_USER_ID, USER_EMAIL, UPDATE_TYPE, CHANGE_TIME, CHANGE_DATE, GROUP_NAME
+            FROM GROUP_MEMBERSHIP
+        ) res_1
+    ) res_2
+    WHERE rn = 1 --only return the last row per user per group per day, de-duping events in a single day to just the most recent event
 ),
 DATE_RANGE AS (
+    --cross join to create a new grain, user per day per group
     SELECT *
     FROM (
         SELECT date_trunc('day', dd)::DATE DATES
-        FROM generate_series ('now'::timestamp - '7 month'::interval, 'now'::timestamp, '1 day'::interval) dd
+        FROM generate_series ('now'::timestamp - '10 month'::interval, 'now'::timestamp, '1 day'::interval) dd
     ) res_1
-    CROSS JOIN (SELECT DISTINCT USER_EMAIL, GROUP_NAME, SYSTEM_USER_ID FROM UNIONED_DATASETS) x
+    CROSS JOIN (SELECT DISTINCT USER_EMAIL, GROUP_NAME, SYSTEM_USER_ID, FIRST_EVENT_DATE FROM UNIONED_DATASETS) x
+    WHERE x.FIRST_EVENT_DATE <= res_1.DATES --only keep rows where the user and group had a relationship
 )
-SELECT 
-VIEW_DATE,
-SYSTEM_USER_ID,
-USER_EMAIL,
-GROUP_NAME,
-FIRST_VALUE(UPDATE_TYPE) OVER(PARTITION BY USER_EMAIL, partition_window ORDER BY VIEW_DATE) UPDATE_TYPE,
-CASE WHEN rn_desc = 1 THEN 1 ELSE 0 END CURRENT_STATUS_FLAG
+SELECT *
 FROM (
     SELECT 
-    dr.DATES VIEW_DATE,
-    dr.SYSTEM_USER_ID,
-    dr.USER_EMAIL,
-    dr.GROUP_NAME,
-    ud.UPDATE_TYPE,
-    SUM(CASE WHEN ud.UPDATE_TYPE IS NULL THEN 0 ELSE 1 END) OVER(PARTITION BY dr.USER_EMAIL, dr.GROUP_NAME ORDER BY dr.DATES) partition_window,
-    ROW_NUMBER() OVER(PARTITION BY dr.USER_EMAIL, dr.GROUP_NAME ORDER BY dr.DATES DESC) rn_desc
-    FROM DATE_RANGE dr
-    LEFT JOIN UNIONED_DATASETS ud ON ud.USER_EMAIL = dr.USER_EMAIL AND ud.GROUP_NAME = dr.GROUP_NAME AND ud.CHANGE_DATE = dr.DATES
-    ORDER BY USER_EMAIL, GROUP_NAME, DATES
-) res_1
+    VIEW_DATE,
+    SYSTEM_USER_ID,
+    USER_EMAIL,
+    GROUP_NAME,
+    FIRST_VALUE(UPDATE_TYPE) OVER(PARTITION BY USER_EMAIL, PARTITION_WINDOW ORDER BY VIEW_DATE) UPDATE_TYPE, --backfill value
+    CASE WHEN rn_desc = 1 THEN 1 ELSE 0 END CURRENT_DAY_FLAG --if the row is the most recent for user then 1
+    FROM (
+        --Subquery is to add all required columns into a single series per user per day per group and adds columns needed for backfill
+        SELECT 
+        dr.DATES VIEW_DATE,
+        dr.SYSTEM_USER_ID,
+        dr.USER_EMAIL,
+        dr.GROUP_NAME,
+        ud.UPDATE_TYPE,
+        SUM(CASE WHEN ud.UPDATE_TYPE IS NULL THEN 0 ELSE 1 END) OVER(PARTITION BY dr.USER_EMAIL, dr.GROUP_NAME ORDER BY dr.DATES) PARTITION_WINDOW, --incremental counter to act as a marker for backfill in outer query
+        ROW_NUMBER() OVER(PARTITION BY dr.USER_EMAIL, dr.GROUP_NAME ORDER BY dr.DATES DESC) rn_desc --row number to find latest row per user per group
+        FROM DATE_RANGE dr
+        LEFT JOIN UNIONED_DATASETS ud ON ud.USER_EMAIL = dr.USER_EMAIL AND ud.GROUP_NAME = dr.GROUP_NAME AND ud.CHANGE_DATE = dr.DATES
+    ) res_1
+    WHERE GROUP_NAME != 'All Users' --removes the generic all users group
+) res_2
+WHERE UPDATE_TYPE != 'REMOVED' --only keep rows if they represent an active membership
 ORDER BY USER_EMAIL, GROUP_NAME, VIEW_DATE
